ea76ff4cfffa9e03c318f21477707ae1d348864d
TheDog21 <justin.e.stach@gmail.com>
2024-04-17 22:48:38 -0400

    changed router

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,10 @@ import { createRouter as _createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../views/HomePage.vue';
 import GamePage from '../views/GamePage.vue';
-import Login from '../views/Login.vue';
 
 const routes = [
   {path: '/', name: 'HomePage', component: HomePage},
   {path: '/Game', name: 'GamePage', component: GamePage},
-  {path: '/Login', name: 'Login', component: Login}
 
 ];
 
