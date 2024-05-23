d461e0ee62e80aac1784548b3d05d4cfc9b3d584
TheDog21 <justin.e.stach@gmail.com>
2024-05-23 15:03:42 -0400

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ReverseMemoryV4.0/',
+  base: '/ReverseMemoryV3.0/',
   plugins: [vue()],
   resolve: {
     alias: {
